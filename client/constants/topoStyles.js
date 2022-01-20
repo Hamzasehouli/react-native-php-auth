afcd0a65ccc47b3077867815db5768e3e561af91
hamzasehouli <sehoulihamza@yahoo.com>
2022-01-20 15:53:51 +0100

    Added fonts, colors, topoStyles and react-native-config

---
+++ client/constants/topoStyles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  fontSize: 16,
+  fontFamilyReg: "Lato-Regular",
+  fontFamilyBold: "Lato-Bold",
+  fontFamilyItalic: "Lato-Italic",
+  fontFamilyLight: "Lato-Light",
+});
